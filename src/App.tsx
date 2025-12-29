4cd6ea64c7c321954f7f11438dce3e26a5424d5e
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2025-12-29 23:05:26 +0700

    feat: implement Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/pages/landing/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
-      <main className="lg:flex lg:flex-col lg:gap-20 lg:px-30 lg:pt-15 lg:pb-30">
+      <main className="lg:flex lg:flex-col lg:gap-20 lg:px-10 lg:pt-15 lg:pb-30 xl:px-30">
         <HeroSection />
       </main>
+      <Footer />
     </>
   );
 }
