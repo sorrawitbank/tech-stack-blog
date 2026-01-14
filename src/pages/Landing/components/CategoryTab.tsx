0ed947652283468d0a185c7d7b547ad7aac6571a
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-01-14 10:41:44 +0700

    style(article): adjust height and padding for CategoryTab

--- src/pages/Landing/components/CategoryTab.tsx
+++ src/pages/Landing/components/CategoryTab.tsx
@@ -9,12 +9,12 @@ interface Props {
 function CategoryTab({ categories, value, onValueChange }: Props) {
   return (
     <Tabs value={value} onValueChange={onValueChange}>
-      <TabsList className="gap-2 bg-brown-200">
+      <TabsList className="gap-2 h-12 py-0 bg-brown-200">
         {categories.map((category) => (
           <TabsTrigger
             key={category}
             value={category}
-            className="h-12 px-5 text-brown-400 rounded-lg hover:bg-brown-300 hover:cursor-pointer data-[state=active]:text-brown-500 data-[state=active]:bg-brown-300"
+            className="h-full px-5 text-brown-400 border-0 rounded-lg hover:bg-brown-300 hover:cursor-pointer data-[state=active]:text-brown-500 data-[state=active]:bg-brown-300"
           >
             <span className="text-body-1">{category}</span>
           </TabsTrigger>
