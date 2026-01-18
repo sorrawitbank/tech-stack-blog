f974d50119e3c5bc27b7e3bac188a5ee68a544c9
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-01-18 20:46:51 +0700

    refactor(layout): restructure Footer and Navbar components to use lists and rename Navbar to Header

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -6,19 +6,23 @@ function Footer() {
     <footer className="flex flex-col items-center gap-6 px-4 py-10 bg-brown-200 sm:flex-row sm:justify-between sm:gap-0 sm:px-12 sm:py-15 xl:px-30">
       <div className="flex gap-6">
         <span className="text-body-1 text-brown-500">Get in touch</span>
-        <div className="flex gap-4">
-          <a className="text-brown-500 hover:text-brown-400 active:text-brown-600">
-            <Linkedin />
-          </a>
-          <a
-            href="https://github.com/sorrawitbank"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-brown-500 hover:text-brown-400 active:text-brown-600"
-          >
-            <Github />
-          </a>
-        </div>
+        <ul className="flex gap-4">
+          <li>
+            <a className="text-brown-500 hover:text-brown-400 active:text-brown-600">
+              <Linkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sorrawitbank"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-brown-500 hover:text-brown-400 active:text-brown-600"
+            >
+              <Github />
+            </a>
+          </li>
+        </ul>
       </div>
       <NavigationButton variant="text">Home page</NavigationButton>
     </footer>
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,12 +1,12 @@
 import ArticleSection from "./ArticleSection";
 import HeroSection from "./HeroSection";
 import Footer from "@/layouts/Footer";
-import Narbar from "@/layouts/Navbar";
+import Header from "@/layouts/Header";
 
 function Landing() {
   return (
     <>
-      <Narbar />
+      <Header />
       <main className="pt-12 sm:pt-20 lg:flex lg:flex-col lg:gap-20 lg:px-12 lg:pt-35 lg:pb-30 xl:px-30">
         <HeroSection />
         <ArticleSection />
