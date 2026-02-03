4353ef95b23ef245010c15510bcabbc5288450e3
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-02-03 15:36:38 +0700

    feat: create reusable Dialog component for improved code organization

--- src/pages/ViewPost/components/CreateAccountDialog.tsx
+++ src/pages/ViewPost/components/CreateAccountDialog.tsx
@@ -1,14 +1,9 @@
 import React, { useContext } from "react";
-import { X } from "lucide-react";
 import { NavigationButton } from "@/components/common/Button";
+import Dialog from "@/components/common/Dialog";
 import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
   AlertDialogDescription,
-  AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { MediaQueryContext } from "@/contexts/MediaQueryContext";
 
@@ -16,41 +11,31 @@ function CreateAccountDialog({ children }: { children?: React.ReactNode }) {
   const { isXLarge } = useContext(MediaQueryContext);
 
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
-      <AlertDialogContent className="flex flex-col gap-6 px-4 pt-4 pb-10 bg-brown-100 border-0 rounded-2xl sm:max-w-120! md:px-6 xl:max-w-155!">
-        <AlertDialogHeader>
-          <AlertDialogCancel className="self-end rounded-full cursor-pointer hover:bg-brown-300">
-            <X />
-          </AlertDialogCancel>
-        </AlertDialogHeader>
-        <div className="flex flex-col items-center gap-4 xl:gap-10">
-          <AlertDialogTitle asChild>
-            <h2
-              className={
-                (isXLarge ? "text-headline-2" : "text-headline-3") +
-                " text-center text-brown-600"
-              }
-            >
-              Create an account to continue
-            </h2>
-          </AlertDialogTitle>
-          <NavigationButton variant="primary" navigateTo="/signup">
-            Create account
-          </NavigationButton>
-          <div className="flex justify-center gap-3">
-            <AlertDialogDescription asChild>
-              <span className="text-body-1 text-brown-400!">
-                Already have an account?
-              </span>
-            </AlertDialogDescription>
-            <NavigationButton variant="text" navigateTo="/login">
-              Login
-            </NavigationButton>
-          </div>
-        </div>
-      </AlertDialogContent>
-    </AlertDialog>
+    <Dialog trigger={children}>
+      <AlertDialogTitle asChild>
+        <h2
+          className={
+            (isXLarge ? "text-headline-2" : "text-headline-3") +
+            " text-center text-brown-600"
+          }
+        >
+          Create an account to continue
+        </h2>
+      </AlertDialogTitle>
+      <NavigationButton variant="primary" navigateTo="/signup">
+        Create account
+      </NavigationButton>
+      <div className="flex justify-center gap-3">
+        <AlertDialogDescription asChild>
+          <span className="text-body-1 text-brown-400!">
+            Already have an account?
+          </span>
+        </AlertDialogDescription>
+        <NavigationButton variant="text" navigateTo="/login">
+          Login
+        </NavigationButton>
+      </div>
+    </Dialog>
   );
 }
 
