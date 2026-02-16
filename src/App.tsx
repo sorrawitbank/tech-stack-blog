8c0e8aa73e8d37085bfbf6a1935d8111f9651925
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-02-16 21:52:05 +0700

    refactor(routes): restructure routing logic by creating a dedicated AppRoutes component, enhancing authentication and role-based access control for user and admin sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
+import { AuthProvider } from "./contexts/AuthContext";
 import { MediaQueryProvider } from "./contexts/MediaQueryContext";
 import { ScrollProvider } from "./contexts/ScrollContext";
-import Landing from "./pages/Landing";
-import ViewPost from "./pages/ViewPost";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import Profile from "./pages/Member/Profile";
-import ResetPassword from "./pages/Member/ResetPassword";
+import AppRoutes from "./routes";
+import jwtInterceptor from "./utils/jwtInterceptor";
+
+jwtInterceptor();
 
 function App() {
   return (
     <Router>
-      <MediaQueryProvider>
-        <ScrollProvider>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/post/:postId" element={<ViewPost />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Toaster />
-        </ScrollProvider>
-      </MediaQueryProvider>
+      <AuthProvider>
+        <MediaQueryProvider>
+          <ScrollProvider>
+            <AppRoutes />
+            <Toaster />
+          </ScrollProvider>
+        </MediaQueryProvider>
+      </AuthProvider>
     </Router>
   );
 }
